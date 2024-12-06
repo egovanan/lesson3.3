c457b45040314a919f6ac8bc620d5758ceb034d4
egovanan <sementsov1128@gmail.com>
2024-12-06 22:23:01 +0300

    add new const

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption('Игра Тир')
+icon = pygame.image.load()
+
 runing = True
 while runing
     pass
 
 pygame.quit()
+
